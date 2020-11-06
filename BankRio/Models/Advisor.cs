7624c1c42a787f72490890fe251b87c76c5b427e
Cartman <42188391+EricCartman01@users.noreply.github.com>
2020-11-06 11:45:26 -0300

    Advisor class

---
+++ BankRio/Models/Advisor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRio.Models
+{
+    public class Advisor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Initials { get; set; }
+        public string XPCode { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<ComissionRecord> Comissions { get; set; }
+
+
+        public Advisor(string name, string initials, string xPCode, string email)
+        {
+            Name = name;
+            Initials = initials;
+            XPCode = xPCode;
+            Email = email;
+        }
+
+        public void AddComissions(ComissionRecord comission)
+        {
+            Comissions.Add(comission);
+        }
+
+        public void RemoveComissions(ComissionRecord comission)
+        {
+            Comissions.Remove(comission);
+        }
+
+        public double TotalComissions(DateTime initial, DateTime final)
+        {
+            return (Comissions.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.ITAZ));
+        }
+
+    }
+}
---
+++ BankRio/Models/ComissionRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRio.Models
+{
+    public class ComissionRecord
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+        public Advisor Advisor { get; set; }
+        public double NetCertification { get; set; }
+        public double Net { get; set; }
+        public double NetBirthday { get; set; }
+        public double NetTotal { get; set; }
+        public double XPC { get; set; }
+        public double CMBC { get; set; }
+        public double PROTC { get; set; }
+        public double ITAZ { get; set; }
+        public double JURC { get; set; }
+        public double PAN { get; set; }
+
+    }
+}
